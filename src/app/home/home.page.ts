e0be367821f7602f7cdac4ae1954f8b52057f110
hannah1306 <suhyun1306@gmail.com>
2020-05-28 06:13:23 +0900

    Get currentPosition and weather

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public lat: number;
+  public long: number;
+  public API_KEY: string = '372365259bda80061591382a4df3b6c8';
 
-  
+  constructor(
+    private http: HttpClient
+  ) {
+  }
 
+  ionViewWillEnter(){
+    this._getCurrentPosition();
+  }
+
+  _getCurrentPosition(){
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.lat = data.coords.latitude;
+      this.long = data.coords.longitude;
+      console.log(this.lat, this.long);
+
+      this._getWeather();
+    })
+  }
+
+  _getWeather(){
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.long}&exclude=hourly,daily&appid=${this.API_KEY}`;
+    this.http.get(url).subscribe(data => {
+      console.log(data);
+    })
+  }
 }
